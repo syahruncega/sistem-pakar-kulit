29b9695378213dd0d34e43a85c77cac81584feb2
syahruncega@gmail.com <syahruncega@gmail.com>
2022-07-17 14:51:07 +0800

    Halaman Materi

---
+++ components/Modal/ModalMateri.tsx
@@ -0,0 +1,56 @@
+import {
+  AspectRatio,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { FC } from "react";
+import NextImage from "next/image";
+
+const ModalMateri: FC<{
+  alt: string;
+  img: string;
+  description: string;
+}> = ({ alt, img, description }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Detail</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent maxW={600}>
+          <ModalHeader>{alt}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <AspectRatio
+              ratio={4 / 3}
+              w="full"
+              borderRadius="md"
+              overflow={"hidden"}
+              mb={4}
+            >
+              <NextImage
+                layout="fill"
+                objectFit="cover"
+                src={img}
+                placeholder={"blur"}
+                alt={alt}
+                blurDataURL={img}
+              />
+            </AspectRatio>
+            {description}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalMateri;
